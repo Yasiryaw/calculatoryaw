db481cf6d2566ec3388f10df150942fdfe70e674
Yasir Elmula <yasir.yaw.89@gmail.com>
2023-12-03 17:39:43 +0000

    for rebase

--- cal.sh
+++ cal.sh
@@ -1,7 +1,13 @@
 hi this is just a test 
 # just to add more stuff. 
 
+<<<<<<< HEAD
 1+2 = 4
 
 
 #percentage 
+=======
+1+2 = 3
+
+#test on calculator 
+>>>>>>> 95e5b9e ( little change for the main)
